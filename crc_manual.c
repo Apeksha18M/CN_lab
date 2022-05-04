df2f8ae9d4ff0e4bffde4c6f405118e40eba6468
Apeksha18M <67169238+Apeksha18M@users.noreply.github.com>
2022-05-05 01:12:52 +0530

    CRC code updated

--- crc_manual.c
+++ crc_manual.c
@@ -1,17 +1,15 @@
-/******************************************************************************
-CRC
-*******************************************************************************/
-
 #include <stdio.h>
 #include <string.h>
 
 char crc[30];
 
-void XOR(char a[30],char generator[30])
+void XOR(char crc[30], char generator[30])
 {
-    char crc[30];
     for(int i=0; i<strlen(generator);i++)
-        crc[i] = a[i]^generator[i];
+    {
+        crc[i] = (crc[i] == generator[i]? '0':'1');
+    }    
+    return ;
 }
 
 int main()
@@ -30,23 +28,22 @@ int main()
 
         msg[i] = '0';
     }
-    msg[i+1] = '\0';
-    printf("meow");
+    msg[i] = '\0';
     printf("\nAfter appending zeros: %s",msg);
-    
     //Performing modulo-2 division
-    for(i=0;i<gen_len-1; i++)
+    for(i=0; i<gen_len; i++)
     {
         crc[i] = msg[i]; //copying the msg to crc
-    }
+    } 
     do{
         if(crc[0]=='1')
             XOR(crc,gen);
         for(j=0;j<gen_len-1;j++)
             crc[j] = crc[j+1];
-        crc[j] = msg[j+1];
+        crc[j] = msg[i++];
     }while(i<=(msg_len+gen_len-1));
-    
+
+    crc[i]='\0';
     printf("\nchecksum is %s",crc);
     
     for(i = 0; i<(gen_len-1); i++)
@@ -56,4 +53,3 @@ int main()
     printf("\nFinal codeword: %s", msg);
     
 }
-
